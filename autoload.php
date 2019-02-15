a45f371fc2e82b6c802b8ad6c82a6796f8965b95
cs-eliseev <a.k.eliseev@gmail.com>
2019-02-15 07:30:41 +0300

    add local autoload

--- autoload.php
+++ autoload.php
@@ -8,5 +8,20 @@ switch (true) {
         require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autoload.php';
         break;
     default:
-        throw new Exception('file autoload.php is not exist');
+        $path = __DIR__ . DIRECTORY_SEPARATOR . 'src';
+
+        $iterator = new DirectoryIterator($path);
+
+        while ($iterator->valid()) {
+            $item = $iterator->current();
+
+            if($item->isFile()) {
+                require_once $path . DIRECTORY_SEPARATOR . $item->getFilename();
+            }
+
+            unset($item);
+            $iterator->next();
+        }
+
+        break;
 }
