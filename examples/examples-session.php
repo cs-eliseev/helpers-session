fa1a21d39f95e3017a7ca292d68c44fa72ac3185
cs-eliseev <a.k.eliseev@gmail.com>
2019-02-23 10:28:00 +0300

    add example view set multi key

--- examples/examples-session.php
+++ examples/examples-session.php
@@ -40,3 +40,48 @@ echo PHP_EOL;
 Session::remove('example_key');
 var_dump(Session::has('example_key'));
 echo PHP_EOL;
+
+
+
+// Example: multiKey
+Session::setMultiKey('cse');
+
+// Example: start
+// true
+var_dump(Session::start());
+echo PHP_EOL;
+
+// Example: set
+// ['example_key' => 'example_value']
+Session::set('example_key', 'example_value');
+echo PHP_EOL;
+
+// Example: has
+// true
+var_dump(Session::has('example_key'));
+echo PHP_EOL;
+
+// Example: get
+// example_value
+var_dump(Session::get('example_key'));
+echo PHP_EOL;
+
+// Example: get not empty
+// default_value
+Session::set('example_default', '');
+var_dump(Session::getNotEmpty('example_default', 'default_value'));
+echo PHP_EOL;
+
+// Example: get all data
+// ['example_key' => 'example_value', 'example_default' => '']
+var_dump(Session::all());
+echo PHP_EOL;
+
+// Example all session data
+var_dump($_SESSION);
+
+// Example: remove
+// false
+Session::remove('example_key');
+var_dump(Session::has('example_key'));
+echo PHP_EOL;
